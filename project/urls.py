58d856d2c856008cbd92c90749aface5db02a12c
andrew-atef <49458507+andrew-atef@users.noreply.github.com>
2022-03-11 00:29:11 +0200

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -19,8 +19,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from django.views.static import serve
-from django.conf.urls import url
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls')),
@@ -28,6 +27,4 @@ urlpatterns = [
     path('affiliate/', include('affiliate.urls')),
     path('tinymce/', include('tinymce.urls')),
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
