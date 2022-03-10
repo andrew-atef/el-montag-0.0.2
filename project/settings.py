51041a02192b8b9a62775f0a2da430243beb9ca4
andrew-atef <49458507+andrew-atef@users.noreply.github.com>
2022-03-11 00:35:36 +0200

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -134,7 +134,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CSRF_TRUSTED_ORIGINS = ['el-montag.herokuapp.com']
+CSRF_TRUSTED_ORIGINS = ['https://el-montag.herokuapp.com']
 
 
 
